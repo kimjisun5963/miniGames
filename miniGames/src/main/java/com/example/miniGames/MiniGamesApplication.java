8d940078e22d5c207ef0529c00842880cd2f63c4
kimjisun <wltjs5963@naver.com>
2024-07-29 23:34:15 +0900

    초기 세팅

---
+++ miniGames/src/main/java/com/example/miniGames/MiniGamesApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniGamesApplication.class, args);
+	}
+
+}
---
+++ miniGames/src/test/java/com/example/miniGames/MiniGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
